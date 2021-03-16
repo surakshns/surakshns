603161929c746d22dc669b3f5e5e798a46f770c3
surakshns <surakshnss@gmail.com>
2021-03-16 23:02:53 +0530

    first hello

---
+++ src/components/hello.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Hello = () =>{
+    return(
+    <div className='container'>
+        <div className='halfscreen'><div className='navbar-band title'>SURAKSH</div><div className='colororg'></div></div>
+        <div className='twohalfscreen'><div className='whitescreen'></div></div>
+        <div className="hello"> 
+            <p className='h'>H</p>
+            <p className='e'>E</p>
+            <p className='l1'>L</p>
+            <p className='l2'>L</p>
+            <p className='o'>O</p>
+            <p className='dot'>.</p>
+        </div>
+        <div className='menu-icon scroll'>
+            <i class="fa fa-long-arrow-down" aria-hidden="true"></i>
+        </div>
+    </div>
+    );
+} 
+
+export default Hello;
